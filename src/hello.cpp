6fc1f3349bd32d2081ebc806a1ef338122cc3377
Raul Costa Junior <raulcostajunior@gmail.com>
2021-10-08 19:55:32 +0200

    clang-formatted file.

--- src/hello.cpp
+++ src/hello.cpp
@@ -29,85 +29,68 @@
 
 const int GreetThemCmd = 100;
 
-class THelloApp : public TApplication
-{
-
-public:
-
+class THelloApp : public TApplication {
+   public:
     THelloApp();
 
-    virtual void handleEvent( TEvent& event );
-    static TMenuBar *initMenuBar( TRect );
-    static TStatusLine *initStatusLine( TRect );
+    virtual void handleEvent(TEvent &event);
+    static TMenuBar *initMenuBar(TRect);
+    static TStatusLine *initStatusLine(TRect);
 
-private:
-
+   private:
     void greetingBox();
 };
 
-THelloApp::THelloApp() :
-    TProgInit( &THelloApp::initStatusLine,
-               &THelloApp::initMenuBar,
-               &THelloApp::initDeskTop
-             )
-{
-}
+THelloApp::THelloApp()
+    : TProgInit(&THelloApp::initStatusLine, &THelloApp::initMenuBar,
+                &THelloApp::initDeskTop) {}
 
-void THelloApp::greetingBox()
-{
-    TDialog *d = new TDialog(TRect( 25, 5, 55, 16 ), "Hello, World!" );
+void THelloApp::greetingBox() {
+    TDialog *d = new TDialog(TRect(25, 5, 55, 16), "Hello, World!");
 
-    d->insert( new TStaticText( TRect( 3, 5, 15, 6 ), "How are you?" ) );
-    d->insert( new TButton( TRect( 16, 2, 28, 4 ), "Terrific", cmCancel, bfNormal ) );
-    d->insert( new TButton( TRect( 16, 4, 28, 6 ), "Ok", cmCancel, bfNormal ) );
-    d->insert( new TButton( TRect( 16, 6, 28, 8 ), "Lousy", cmCancel, bfNormal ) );
-    d->insert( new TButton( TRect( 16, 8, 28, 10 ), "Cancel", cmCancel, bfNormal ) );
+    d->insert(new TStaticText(TRect(3, 5, 15, 6), "How are you?"));
+    d->insert(new TButton(TRect(16, 2, 28, 4), "Terrific", cmCancel, bfNormal));
+    d->insert(new TButton(TRect(16, 4, 28, 6), "Ok", cmCancel, bfNormal));
+    d->insert(new TButton(TRect(16, 6, 28, 8), "Lousy", cmCancel, bfNormal));
+    d->insert(new TButton(TRect(16, 8, 28, 10), "Cancel", cmCancel, bfNormal));
 
-    deskTop->execView( d );
+    deskTop->execView(d);
     destroy(d);
 }
 
-void THelloApp::handleEvent( TEvent& event )
-{
-    TApplication::handleEvent( event );
-    if( event.what == evCommand )
-        {
-        switch( event.message.command )
-            {
+void THelloApp::handleEvent(TEvent &event) {
+    TApplication::handleEvent(event);
+    if (event.what == evCommand) {
+        switch (event.message.command) {
             case GreetThemCmd:
                 greetingBox();
-                clearEvent( event );
+                clearEvent(event);
                 break;
             default:
                 break;
-            }
         }
+    }
 }
 
-TMenuBar *THelloApp::initMenuBar( TRect r )
-{
+TMenuBar *THelloApp::initMenuBar(TRect r) {
+    r.b.y = r.a.y + 1;
 
-    r.b.y = r.a.y+1;
-
-    return new TMenuBar( r,
-      *new TSubMenu( "~H~ello", kbAltH ) +
-        *new TMenuItem( "~G~reeting...", GreetThemCmd, kbAltG ) +
-         newLine() +
-        *new TMenuItem( "E~x~it", cmQuit, cmQuit, hcNoContext, "Alt-X" )
-        );
-
+    return new TMenuBar(
+        r, *new TSubMenu("~H~ello", kbAltH) +
+               *new TMenuItem("~G~reeting...", GreetThemCmd, kbAltG) +
+               newLine() +
+               *new TMenuItem("E~x~it", cmQuit, cmQuit, hcNoContext, "Alt-X"));
 }
 
-TStatusLine *THelloApp::initStatusLine( TRect r )
-{
-    r.a.y = r.b.y-1;
-    return new TStatusLine( r,
-        *new TStatusDef( 0, 0xFFFF ) +
-            *new TStatusItem( "~Alt-X~ Exit", kbAltX, cmQuit ) + *new TStatusItem( 0, kbF10, cmMenu ));
+TStatusLine *THelloApp::initStatusLine(TRect r) {
+    r.a.y = r.b.y - 1;
+    return new TStatusLine(
+        r, *new TStatusDef(0, 0xFFFF) +
+               *new TStatusItem("~Alt-X~ Exit", kbAltX, cmQuit) +
+               *new TStatusItem(0, kbF10, cmMenu));
 }
 
-int main()
-{
+int main() {
     THelloApp helloWorld;
     helloWorld.run();
     return 0;
